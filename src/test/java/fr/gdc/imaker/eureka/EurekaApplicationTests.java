6d2584d9019e4b6f9886c5a2aee8f423acec3c9e
ahmed <ahmed.arbi@tek-up.de>
2021-06-30 16:10:19 +0100

    setup the discovery service with eureka netflix

---
+++ src/test/java/fr/gdc/imaker/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.gdc.imaker.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
