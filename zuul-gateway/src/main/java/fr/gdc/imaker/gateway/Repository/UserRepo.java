4f34a6651e17a2646714ec08f68b49a5def59754
AhmedArbi <57601918+AhmedArbi@users.noreply.github.com>
2021-07-23 09:47:16 +0100

    Delete zuul-gateway directory

--- zuul-gateway/src/main/java/fr/gdc/imaker/gateway/Repository/UserRepo.java
+++
@@ -1,8 +0,0 @@
-package fr.gdc.imaker.gateway.Repository;
-
-import fr.gdc.imaker.gateway.Model.Users;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<Users,Long> {
-    Users findByEmail(String email);
-}
